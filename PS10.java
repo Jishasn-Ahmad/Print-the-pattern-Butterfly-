9f1a11bb0150893f262a8d4edc316cae13630ed9
Jishan Ahmad <139096572+Jishasn-Ahmad@users.noreply.github.com>
2024-05-24 16:29:43 +0530

    Add files via upload

---
+++ PS10.java
@@ -0,0 +1,43 @@
+public class PS10 {
+    public static void main(String[] args) {
+        //butterfly
+
+        int n=5;
+        //upper half
+
+        for(int i=1; i<=n; i++){
+            // first part
+            for(int j=1;j<=i; j++){
+                System.out.print("*");
+            }
+            //spaces
+            int spaces = 2*(n-i);
+            for(int j=1; j<=spaces; j++){
+                System.out.print(" ");
+            }
+            //2nd part
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //lower half
+        for(int i=n; i>=1; i--){
+            //first part
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            //spaces
+            int spaces = 2*(n-i);
+            for(int j=1; j<=spaces;j++){
+                System.out.print(" ");
+            }
+            //2nd part
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
